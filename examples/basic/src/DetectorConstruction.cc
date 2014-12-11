48d1f08e2af5635da80d71e2ab75f75a3822ddde
Christopher Poole <mail@christopherpoole.net>
2014-12-11 12:44:18 +1100

    Setting the world in the example to be invisable, and added a color to one of the loaded meshes.

--- examples/basic/src/DetectorConstruction.cc
+++ examples/basic/src/DetectorConstruction.cc
@@ -27,6 +27,8 @@
 
 #include "G4NistManager.hh"
 #include "G4Material.hh"
+#include "G4VisAttributes.hh"
+
 
 DetectorConstruction::DetectorConstruction()
 {;}
@@ -45,22 +47,35 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
     world_logical = new G4LogicalVolume(world_solid, air,"world_logical",0,0,0);
     world_physical = new G4PVPlacement(0, G4ThreeVector(), world_logical,
                                        "world_physical", 0, false, 0);
+    world_logical->SetVisAttributes(G4VisAttributes::Invisible);
+
+    // CAD model rotation. 
+    G4RotationMatrix * rot = new G4RotationMatrix();
+    rot->rotateZ(90*deg);
     
     // Load CAD file as tessellated solid //
-    offset = G4ThreeVector(-30*cm, 0, 0);
-    CADMesh * mesh = new CADMesh("../../models/cone.ply", "PLY", mm, offset, false);
+    offset = G4ThreeVector(-20*cm, 0, 0);
+    
+    // Note that offset is applied to the points in mesh directly before placement.
+    CADMesh * mesh = new CADMesh("../../models/cone.ply", "PLY");
+    mesh->SetScale(mm);
+    mesh->SetOffset(offset);
+    mesh->SetReverse(false);
 
     cad_solid = mesh->TessellatedMesh();
     cad_logical = new G4LogicalVolume(cad_solid, water, "cad_logical", 0, 0, 0);
-    cad_physical = new G4PVPlacement(0, G4ThreeVector(), cad_logical,
+    cad_physical = new G4PVPlacement(rot, G4ThreeVector(), cad_logical,
                                      "cad_physical", world_logical, false, 0);
+    cad_logical->SetVisAttributes(G4Color(0.5, 0.3, 1, 1));
     
     // Load CAD file as tetrahedral mesh //
-    CADMesh * tet_mesh = new CADMesh("../../models/cone.ply", "PLY", water);
+    CADMesh * tet_mesh = new CADMesh("../../models/cone.ply", "PLY");
+    tet_mesh->SetScale(1.5);
+    tet_mesh->SetMaterial(water); // We have to do this before making the G4AssemblyVolume.
+
     G4AssemblyVolume * cad_assembly = tet_mesh->TetrahedralMesh();
 
-    G4Translate3D translation(30*cm, 0., 0.);
-    G4RotationMatrix * rot = new G4RotationMatrix();
+    G4Translate3D translation(20*cm, 0., 0.);
     G4Transform3D rotation = G4Rotate3D(*rot);
     G4Transform3D transform = translation*rotation;
 
